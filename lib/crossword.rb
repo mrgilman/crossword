afbdc131dbc49829a0c5a7770d461457a0078192
Melanie Gilman <melanie.gilman@gmail.com>
2013-08-31 13:09:26 -0400

    Fix file loading

--- lib/crossword.rb
+++ lib/crossword.rb
@@ -1,8 +1,8 @@
-require 'crossword/grid'
-require 'crossword/cell'
-require 'crossword/word_list'
-require 'crossword/puzzle'
-require 'crossword/loader'
+require_relative 'crossword/grid'
+require_relative 'crossword/cell'
+require_relative 'crossword/word_list'
+require_relative 'crossword/puzzle'
+require_relative 'crossword/loader'
 
 module Crossword
 end
