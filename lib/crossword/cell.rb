755cff727b5b2c02bae231ab6855ea0e02ea3d95
Melanie Gilman <melanie.gilman@gmail.com>
2013-08-31 15:56:04 -0400

    Change attr_reader to accessor

--- lib/crossword/cell.rb
+++ lib/crossword/cell.rb
@@ -1,14 +1,11 @@
 module Crossword
   class Cell
-    attr_reader :x, :y, :letter
+    attr_reader   :x, :y
+    attr_accessor :letter
 
     def initialize(x, y)
-      @x      = x
-      @y      = y
-    end
-
-    def letter=(letter)
-      @letter = letter
+      @x = x
+      @y = y
     end
   end
 end
