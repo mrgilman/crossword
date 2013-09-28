9923f5201e713bbd59d6fe2e61019b67336c99e1
Melanie Gilman <melanie.gilman@gmail.com>
2013-09-28 15:43:56 -0400

    Add publicly available list of 4 letter words

--- lib/crossword/loader.rb
+++ lib/crossword/loader.rb
@@ -3,8 +3,9 @@ module Crossword
 
     ALL = 'word_lists/all.txt'
     NYT = 'word_lists/nyt.txt'
+    FLW = 'word_lists/four_letter_words.txt'
 
-    def self.load_word_list_file(file=ALL)
+    def self.load_word_list_file(file=FLW)
       words = File.open(file).read
       words.split("\n")
     end
