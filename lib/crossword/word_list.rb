00e0eca07d35e71149abecabedf650e10c2c974a
Melanie Gilman <melanie.gilman@gmail.com>
2013-09-28 15:16:57 -0400

    Delete unused method

--- lib/crossword/word_list.rb
+++ lib/crossword/word_list.rb
@@ -10,10 +10,6 @@ module Crossword
       WordList.new(words.select{|w| w.length == length})
     end
 
-    def letter_in_position(letter, position)
-      WordList.new(words.select{|w| w[position - 1] == letter})
-    end
-
     def pick_word(length)
       of_length(length).words.sample
     end
